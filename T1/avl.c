0b7555f155438f18438584ddf5ae29bbd57a4dc3
Gabriel Vassoler <gabiv98@gmail.com>
2017-04-21 14:57:20 -0300

    batata

--- T1/avl.c
+++ T1/avl.c
@@ -2,121 +2,173 @@
 #include <stdlib.h>
 
 typedef struct _nodo{
-	int chave;
-	int altura, nivel;
-	int altdireita,altesquerda;
-	struct _nodo *esq;
-	struct _nodo *dir;
-	struct _nodo *pai;
+    int chave;
+    int altura, nivel;
+    int altdireita,altesquerda;
+    struct _nodo *esq;
+    struct _nodo *dir;
+    struct _nodo *pai;
 } TpNodo;
 
 typedef struct _arvore{
-		TpNodo *raiz;
+    TpNodo *raiz;
 } TpArvore;
 
-TpArvore *inicializa(){//aloca memoria para inicializar a arvore
-	TpArvore *arvore=(TpArvore*)malloc(sizeof(TpArvore));
-	arvore->raiz=NULL;
-	return arvore;
+TpArvore *inicializa();
+
+void imprime(TpArvore *tree, TpNodo *node);
+
+TpNodo *inserir(TpNodo * node, int valor, int nivel);
+
+TpNodo *rot_dir(TpNodo *node);
+
+TpNodo *rot_esq(TpNodo *node);
+
+int altura(TpNodo *node);
+
+int max(int a, int b);
+
+TpNodo* criarnodo(int valor, int nivel);
+
+int main(){
+    TpArvore *arvore=(TpArvore*)malloc(sizeof(TpArvore));
+    arvore=inicializa();
+
+    int escolha = 1,valor;
+
+    do{
+        printf("Escolha a opção:\n");
+        printf("1-Inserir\n");
+        printf("2-Listar nodos\n");
+        printf("0-Sair\n");
+
+        scanf("%d",&escolha);
+
+        switch(escolha){
+            case 1:
+                printf("Digite o valor a ser inserido\n");
+                scanf("%d",&valor);
+                arvore->raiz = inserir(arvore->raiz,valor,0);
+                break;
+            case 2:
+                imprime(arvore,arvore->raiz);
+                break;
+            case 0:
+                break;
+            default:
+                printf("Valor inválido\n");
+        }
+    }while(escolha != 0);
+
+    return 0;
 }
 
-void imprime ( TpArvore *tree, TpNodo *node) {    
-    if ( tree == NULL || node == NULL ) return; 
+TpNodo *inserir(TpNodo * node, int valor,int nivel){
+
+    if (node == NULL)
+        return(criarnodo(valor,nivel));
+ 
+    if (valor < node->chave)
+        node->esq  = inserir(node->esq, valor,nivel + 1);
+    else if (valor > node->chave)
+        node->dir = inserir(node->dir, valor, nivel + 1);
+    else{
+    	printf("Valor já existente na árvore.\n");
+        return node;
+    }
+ 
+    node->altura = altura(node);
+
+    int balanceamento = node->altesquerda - node->altdireita;
+ 
+    // LL
+    if (balanceamento > 1 && valor < node->esq->chave)
+        return rot_dir(node);
+ 
+    // RR
+    if (balanceamento < -1 && valor > node->dir->chave)
+        return rot_esq(node);
+ 
+    // LR
+    if (balanceamento > 1 && valor > node->esq->chave){
+        node->esq =  rot_esq(node->esq);
+        return rot_dir(node);
+    }
+ 
+    // RL
+    if (balanceamento < -1 && valor < node->dir->chave){
+        node->dir = rot_dir(node->dir);
+        return rot_esq(node);
+    }
+ 
+    return node;
+
+}
+
+TpArvore *inicializa(){//aloca memoria para inicializar a arvore
+    TpArvore *arvore=(TpArvore*)malloc(sizeof(TpArvore));
+    arvore->raiz=NULL;
+    return arvore;
+}
+
+void imprime ( TpArvore *tree, TpNodo *node) {
+    if ( tree == NULL || node == NULL ) return;
     imprime (tree, node->esq);
-    printf("Chave: %d Altura: %d AltEsq: %d AltDir: %d, ",node->chave,node->altura,node->altesquerda,node->altdireita) ;
+    printf("Chave: %d Altura: %d AltEsq: %d AltDir: %d\n",node->chave,node->altura,node->altesquerda,node->altdireita) ;
     imprime (tree, node->dir);
 }
 
-TpArvore *inserir(TpArvore * tree, int valor){
-	TpNodo * new = (TpNodo *) calloc(1,sizeof(TpNodo));
-	int lado = 0;
+TpNodo *rot_esq(TpNodo *node){
+    TpNodo *x = node->dir;
+    TpNodo *y = x->esq;
+ 
+    x->dir = node;
+    node->esq = y;
 
-	if (tree->raiz == NULL){
-		new->chave = valor;
-		new->altura = 0;
-		new->nivel = 0;
-		new->altdireita = 0;
-		new->altesquerda = 0;
-		new->esq = NULL;
-		new->dir = NULL;
-		new->pai = NULL;
-		tree->raiz = new;
-		return tree;
-	}else{
-		TpNodo *aux = tree->raiz;
+    node->altura = altura(node);
+    x->altura = altura(x);
 
-		while(1){
-			if (valor > aux->chave){
-				if (aux->dir == NULL){
-					break;
-				}else{
-					aux = aux->dir;
-				}
-			}else{
-				if (aux->esq == NULL){
-					break;
-				}else{
-					aux = aux->esq;
-				}
-			}
-		}
-
-		if (valor > aux->chave){
-			aux->dir = new;
-			lado = 1;
-		}else{
-			aux->esq = new;
-		}
-
-		new->chave = valor;
-		new->pai = aux;
-		new->nivel = aux->nivel + 1;
-		new->altura = 0;
-		new->altdireita = 0;
-		new->altesquerda = 0;
-
-		while(aux != NULL){
-			aux->altura++;
-			if (lado == 1)
-				aux->altdireita++;
-			else
-				aux->altesquerda++;
-			aux = aux->pai;
-		}
-		return tree;
-
-	}
-
+    return x;
 }
 
-int main(){
-	TpArvore *arvore=(TpArvore*)malloc(sizeof(TpArvore));
-	arvore=inicializa();
+TpNodo *rot_dir(TpNodo *node){
+   	TpNodo *x = node->dir;
+    TpNodo *y = x->esq;
+ 
+    x->esq = node;
+    node->dir = y;
 
-	int escolha = 1,valor;
+    node->altura = altura(node);
+    x->altura = altura(node);
 
-	do{
-		printf("Escolha a opção:\n");
-		printf("1-Inserir\n");
-		printf("2-Listar nodos\n");
-		printf("0-Sair\n");
+    return x;
+}
 
-		scanf("%d",&escolha);
+int max(int a, int b){
+    return (a > b)? a : b;
+}
 
-		switch(escolha){
-			case 1:
-				printf("Digite o valor a ser inserido\n");
-				scanf("%d",&valor);
-				arvore = inserir(arvore,valor);
-				break;
-			case 2:
-				imprime(arvore,arvore->raiz);
-				break;
-			case 0:
-				break;
-			default:
-				printf("Valor inválido\n");
-		}
-	}while(escolha != 0);
+int altura(TpNodo *node) {
+	node->altdireita = 0;
+	node->altesquerda = 0;
+	node->altura = 0;
+
+	if(node->esq != NULL) node->altesquerda = altura(node->esq);
+	if(node->dir != NULL) node->altdireita = altura(node->dir);
+
+	return max(node->altesquerda,node->altdireita);
+}
+
+TpNodo* criarnodo(int valor, int nivel){
+    TpNodo * node = (TpNodo *) calloc(1,sizeof(TpNodo));
+
+    node->chave = valor;
+    node->esq = NULL;
+    node->dir = NULL;
+    node->altura = 0;
+    node->nivel = nivel;
+    node->altesquerda = 0;
+    node->altdireita = 0;   // new node is initially added at leaf
+    return(node);
 }
 
