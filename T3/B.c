6bd4a5abcf8edbdad25d6b4715748c3bc5ea8326
Gabriel Vassoler <gabiv98@gmail.com>
2017-07-01 19:31:46 -0300

    Banana

--- T3/B.c
+++ T3/B.c
@@ -22,6 +22,8 @@ typedef struct tparvore {
 
 Node * crianodo(Key * keys, Node **filhos);
 void insere(int value, Tree *tree, Node *node);
+void inseresplit(int value, Node *node,Node *esq, Node *dir);
+void split(Tree *tree, Node *node);
 
 int main(){
 	Tree *tree=(Tree*)malloc(sizeof(Tree));
@@ -31,7 +33,12 @@ int main(){
 
 	insere(10,tree,tree->raiz);
 	insere(20,tree,tree->raiz);
+	insere(30,tree,tree->raiz);
+	insere(40,tree,tree->raiz);
 	printf("%d\n",tree->raiz->keys->chave);
+	Node * tes = tree->raiz->filhos[0];
+	printf("%p\n",tes->keys);
+	printf("%d\n",tree->raiz->filhos[1]->keys->chave);
 
 	return 0;
 }
@@ -73,8 +80,87 @@ void insere(int value, Tree *tree, Node *node){
 					aux->ant = novo;
 				node->quant++;
 			}else{
-				printf("precisa de split\n");
+				split(tree,node);
+				insere(value,tree,tree->raiz);
 			}
 		}
 	}
+}
+
+void split(Tree *tree, Node *node){
+	if(node->pai != NULL && node->pai->quant == ((2*t)-1)){
+		split(tree,node->pai);
+	}
+	Key *chesq = NULL, *chdir = NULL;
+	chesq = node->keys;
+	Key *aux = chesq;
+	int a = 0;
+
+	for(;a < t-1;a++){
+		aux = aux->prox;
+	}
+	aux->ant->prox = NULL;
+	chdir = aux->prox;
+	aux->prox->ant = NULL;
+
+	Node *filesq[t/2], *fildir[t/2];
+	if(node->filhos != NULL){
+		int i = 0;
+		for(;i < t;i++)
+			filesq[i] = node->filhos[i];
+		for(;i < 2*t;i++)
+			fildir[i] = node->filhos[i];
+	}
+
+	Node *esq = crianodo(chesq,filesq);
+	Node *dir = crianodo(chdir,fildir);
+
+	if(node->pai != NULL){
+		esq->pai = dir->pai = node->pai;
+		inseresplit(aux->chave,node->pai,esq,dir);
+	}else{
+		Node * filhos[2*t];
+		filhos[0] = esq;
+		filhos[1] = dir;
+		Node *raiznova = crianodo(NULL,filhos);
+		Key *key = (Key *) calloc(1,sizeof(Key));
+		key->chave = aux->chave;
+		key->ant = NULL;
+		key->prox = NULL;
+		raiznova->keys = key;
+		raiznova->quant = 1;
+		raiznova->pai = NULL;
+		tree->raiz = raiznova;
+
+		esq->pai = dir->pai = tree->raiz;
+	}
+
+}
+
+void inseresplit(int value, Node *node,Node *esq, Node *dir){
+	int i = 0,a;
+	Key *aux = node->keys, *ant = NULL;
+	for(;aux != NULL && aux->chave < value;aux = aux->prox){
+		ant = aux;
+		i++;
+	}
+	Key *novo = (Key *) calloc(1,sizeof(Key));
+	novo->chave = value;
+	novo->ant = ant;
+	novo->prox = aux;
+	if(ant == NULL)
+		node->keys = novo;
+	else
+		ant->prox = novo;
+	if(aux != NULL)
+		aux->ant = novo;
+	node->quant++;
+	a = i;
+	while(node->filhos[a] != NULL)
+		a++;
+	for(; i != a; a--){
+		node->filhos[a] = node->filhos[a-1];
+	}
+	node->filhos[i] = esq;
+	node->filhos[i+1] = esq;
 }
